a5d3e26a58bbbc0221383a68378e17cefbd641c9
forhot2000 <wx1983@gmail.com>
2015-04-26 23:05:47 +0800

    add method

--- src/main/java/org/example/MyUnit.java
+++ src/main/java/org/example/MyUnit.java
@@ -5,4 +5,8 @@ public class MyUnit {
     public String concatenate(String one, String two){
         return one + two;
     }
+
+    public String[] split(String str){
+    	return str.split(" ");
+    }
 }
